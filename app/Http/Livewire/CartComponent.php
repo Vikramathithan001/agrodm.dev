30126ac2e0cd4d27710e0eb690e7cc5ca278f8e9
Vikramathithan001 <vikramathithan2002@gmail.com>
2024-06-21 17:43:37 +0530

    for hosting the website

---
+++ app/Http/Livewire/CartComponent.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Cart;
+use Illuminate\Support\Facades\Auth;
+class CartComponent extends Component
+{
+    
+    public function increaseQuantity($rowId)
+    {
+        $product = Cart::instance('cart')->get($rowId);
+        $qty     = $product->qty + 1;
+        Cart::instance('cart')->update($rowId,$qty);
+        $this->emitTo('cart-icon-component','refreshComponent');
+    }
+    public function decreaseQuantity($rowId)
+    {
+        $product = Cart::instance('cart')->get($rowId);
+        $qty     = $product->qty - 1;
+        Cart::instance('cart')->update($rowId,$qty);
+        $this->emitTo('cart-icon-component','refreshComponent');
+
+    }
+    public function destroy($id)
+    {
+        Cart::instance('cart')->remove($id);
+        $this->emitTo('cart-icon-component','refreshComponent');
+        session()->flash('success_message','Item has been removed!');
+
+    }
+    public function clearAll()
+    {
+        Cart::instance('cart')->destroy();
+        $this->emitTo('cart-icon-component','refreshComponent');
+
+    }
+    public function checkout()
+    {
+        if(Auth::check())
+        {
+            return redirect()->route('shop.checkout');
+        }
+        else
+        {
+            return redirect()->route('login');
+        }
+    }
+    public function setAmountForCheckOut()
+    {
+        if(!Cart::instance('cart')->count() > 0)
+        {
+            session()->forget('checkout');
+            return;
+        }
+        session()->put('checkout',[
+            'discount'=>0,
+            'subtotal' =>Cart::instance('cart')->subtotal(),
+            'tax'=>Cart::instance('cart')->tax(),
+            'total'=>Cart::instance('cart')->total()
+        ]);
+    }
+   
+    public function render()
+    {
+        $this->setAmountForCheckOut();
+        return view('livewire.cart-component');
+    }
+}
